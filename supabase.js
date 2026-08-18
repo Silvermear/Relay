c614e92217368a41c78ed160036123d31b8c1880
CodeJester | Relay Creator <konamibelasi64@gmail.com>
2026-08-18 23:55:31 +0300

    Delete supabase.js

--- supabase.js
+++
@@ -1,8 +0,0 @@
-const { createClient } = require('@supabase/supabase-js');
-
-const supabaseUrl = 'https://kdxcxosppuzudpuxaeac.supabase.co';
-const supabaseKey = 'sb_publishable_RNxqswxz9c-6SJ3EPmaHpg_DYAlFW5S';
-
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-module.exports = supabase;
